4a50d6d1c58f0a7c98f7eadcda9f77849f5c141e
Damian0222 <d00229247@student.dkit.ie>
2024-12-01 14:25:15 +0000

    Added all the remaining comments and changed the logo picture

--- DamianCA2/Services/MovieService.cs
+++ DamianCA2/Services/MovieService.cs
@@ -10,29 +10,31 @@ namespace DamianCA2.Services
     public class MovieService
     {
         private readonly HttpClient httpClient;
-        private readonly string apiKey = "9fb03db06401769da1426077f6d5fd9c"; // Replace with your TMDb API key
-
+        // API key to MovieDb
+        private readonly string apiKey = "9fb03db06401769da1426077f6d5fd9c";
+        // Constructor for MovieService
         public MovieService(HttpClient httpClient)
-        {
+        {// Puts the provided HttpClient to a private field
             this.httpClient = httpClient;
 
             if (string.IsNullOrWhiteSpace(apiKey))
-            {
+            {  // Throws an exception if the API key is wrong
                 throw new Exception("No API key added, please try again");
             }
-        }
-        public async Task<string> FetchMusicData(string query)
-        {
+        } 
+       // Async method to fetch news articles
+           public async Task<string> FetchMovieData(string query){
             try
-            {
+            {  // URL for the movieDB API request and API key.
                 var url = $"https://api.themoviedb.org/3/search/movie?api_key={apiKey}&query={Uri.EscapeDataString(query)}";
-
                 Console.WriteLine($"Searching: {url}");
+                // Sends an getasync request to the NewsAPI 
                 var response = await httpClient.GetAsync(url);
-
+                // Checksthere is an an error.
                 if (!response.IsSuccessStatusCode)
                 {
                     Console.WriteLine($"Api error: {response.StatusCode}");
+                    // Reads error message from the response
                     var errorDetails = await response.Content.ReadAsStringAsync();
                     Console.WriteLine($"Error details: {errorDetails}");
                     return null;
--- DamianCA2/Services/NewsService.cs
+++ DamianCA2/Services/NewsService.cs
@@ -24,7 +24,7 @@ public class NewsService
         }
     }
     // Async method to fetch news articles
-    public async Task<string> FetchMovieNewsAsync()
+    public async Task<string> FetchMovie()
     {
         try
         {
